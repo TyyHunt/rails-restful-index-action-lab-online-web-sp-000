fd907b92e1387180c6de1406d64037fdf51b76db
Tyler Hunt <tyhunt04@gmail.com>
2019-04-24 16:24:42 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,9 +1,5 @@
-class StudentsController < ApplicationController
+class Student < ApplicationController
   def to_s
     self.first_name + " " + self.last_name
-  end
-
-  def index
-    @students = Student.all
   end
 end
